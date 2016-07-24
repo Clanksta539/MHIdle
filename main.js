444b2e8195e226f8ec1c59539cb1b6452d323cbc
Clanksta539 <clankyboy@gmail.com>
2016-07-24 17:40:58 +0100

    Rename Variables

--- main.js
+++ main.js
@@ -1,26 +1,26 @@
-var cookies = 0;
+var resources = 0;
 
-function cookieClick(number){
-    cookies = cookies + number;
-    document.getElementById("cookies").innerHTML = cookies;
+function gatherClick(number){
+	resources = resources + number;
+    document.getElementById("resources").innerHTML = resources;
 };
 
-var cursors = 0;
+var gatherers = 0;
 
-function buyCursor(){
-    var cursorCost = Math.floor(10 * Math.pow(1.1,cursors));     //works out the cost of this cursor
-    if(cookies >= cursorCost){                                   //checks that the player can afford the cursor
-        cursors = cursors + 1;                                   //increases number of cursors
-        cookies = cookies - cursorCost;                          //removes the cookies spent
-        document.getElementById('cursors').innerHTML = cursors;  //updates the number of cursors for the user
-        document.getElementById('cookies').innerHTML = cookies;  //updates the number of cookies for the user
+function trainCat(){
+    var trainingCost = Math.floor(10 * Math.pow(1.1,gatherers));     //works out the cost of this cursor
+    if(resources >= trainingCost){                                   //checks that the player can afford the cursor
+        gatherers = gatherers + 1;                                   //increases number of gatherers
+        resources = resources - trainingCost;                          //removes the resources spent
+        document.getElementById('gatherers').innerHTML = gatherers;  //updates the number of gatherers for the user
+        document.getElementById('resources').innerHTML = resources;  //updates the number of resources for the user
     };
-    var nextCost = Math.floor(10 * Math.pow(1.1,cursors));       //works out the cost of the next cursor
-    document.getElementById('cursorCost').innerHTML = nextCost;  //updates the cursor cost for the user
+    var nextCost = Math.floor(10 * Math.pow(1.1,gatherers));       //works out the cost of the next cursor
+    document.getElementById('trainingCost').innerHTML = nextCost;  //updates the cursor cost for the user
 };
 
 window.setInterval(function(){
 
-    cookieClick(cursors);
+    gatherClick(gatherers);
 
 }, 1000);
