ac7e32f0625c41c91e7ca05a4b79a82c3095bcf9
Josh <josh>
2016-07-24 19:09:31 +0100

    Added hire felynes

--- main.js
+++ main.js
@@ -1,11 +1,23 @@
 var resources = 0;
 var felynes = 1; //these cats do what you tell them.
+var gatherers = 0; //these are automatic cats.
+
 function gatherClick(number){
 	resources = resources + number;
     document.getElementById("resources").innerHTML = resources;
 };
 
-var gatherers = 0; //these are automatic cats.
+function hireCat() {//hiring spare cats
+    var hireCost = Math.floor(10 * Math.pow(1.1, felynes));
+    if (resources >= hireCost) {
+        felynes = felynes + 1;
+        resources = resources - hireCost;
+        document.getElementById('felynes').innerHTML = felynes;
+        document.getElementById('resources').innerHTML = resources;
+        var nextCost = Math.floor(10 * Math.pow(1.1, felynes));
+        document.getElementById('hiringCost').innerHTML = nextCost;
+    }
+}
 
 function trainCat(){
     var trainingCost = Math.floor(10 * Math.pow(1.1,gatherers));     
@@ -13,7 +25,8 @@ function trainCat(){
         gatherers = gatherers + 1; 
         resources = resources - trainingCost;
         felynes = felynes - 1; //removes from spare pool
-        document.getElementById('gatherers').innerHTML = gatherers;  
+        document.getElementById('gatherers').innerHTML = gatherers;
+        document.getElementById('felynes').innerHTML = felynes;
         document.getElementById('resources').innerHTML = resources;  
     };
     var nextCost = Math.floor(10 * Math.pow(1.1,gatherers)); 
